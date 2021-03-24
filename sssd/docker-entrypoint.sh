9c8b82709afb15cdd4c7a0f7a6e0d2f29e897652
arisjr <aristeu.jr@gmail.com>
2021-03-24 11:50:49 -0300

    Update docker-entrypoint.sh

--- sssd/docker-entrypoint.sh
+++ sssd/docker-entrypoint.sh
@@ -297,20 +297,20 @@ crudini --set $SAMBA_CONF global "dedicated keytab file" "$DEDICATED_KEYTAB_FILE
 crudini --set $SAMBA_CONF global "kerberos method" "$KERBEROS_METHOD"
 
 
-# home shared directory (restricted to owner)
-crudini --set $SAMBA_CONF homes "comment" "Home Directories"
-crudini --set $SAMBA_CONF homes "path" "/home/%u/Desktop"
-crudini --set $SAMBA_CONF homes "public" "no"
-crudini --set $SAMBA_CONF homes "guest ok" "no"
-crudini --set $SAMBA_CONF homes "read only" "no"
-crudini --set $SAMBA_CONF homes "writeable" "yes"
-crudini --set $SAMBA_CONF homes "create mask" "0777"
-crudini --set $SAMBA_CONF homes "directory mask" "0777"
-crudini --set $SAMBA_CONF homes "browseable" "no"
-crudini --set $SAMBA_CONF homes "printable" "no"
-crudini --set $SAMBA_CONF homes "oplocks" "yes"
-crudini --set $SAMBA_CONF homes "valid users" "%S"
-crudini --set $SAMBA_CONF homes "hide unreadable" "yes"
+## home shared directory (restricted to owner)
+#crudini --set $SAMBA_CONF homes "comment" "Home Directories"
+#crudini --set $SAMBA_CONF homes "path" "%H"
+#crudini --set $SAMBA_CONF homes "public" "no"
+#crudini --set $SAMBA_CONF homes "guest ok" "no"
+#crudini --set $SAMBA_CONF homes "read only" "no"
+#crudini --set $SAMBA_CONF homes "writeable" "yes"
+#crudini --set $SAMBA_CONF homes "create mask" "0777"
+#crudini --set $SAMBA_CONF homes "directory mask" "0777"
+#crudini --set $SAMBA_CONF homes "browseable" "no"
+#crudini --set $SAMBA_CONF homes "printable" "no"
+#crudini --set $SAMBA_CONF homes "oplocks" "yes"
+#crudini --set $SAMBA_CONF homes "valid users" "%S"
+#crudini --set $SAMBA_CONF homes "hide unreadable" "yes"
 
 # # public shared directory (unrestricted)
 # mkdir -p "/usr/share/public"
